acc560122c16d8fd3108aa5a0a21d7607d190153
sinsang2752 <sinsang2752@naver.com>
2024-03-30 14:52:12 +0900

    Update Ps_1436_현상.java

--- week_01/01_Brute_Force/Ps_1436_현상.java
+++ week_01/01_Brute_Force/Ps_1436_현상.java
@@ -1,1 +1,23 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		int input = Integer.parseInt(br.readLine());
+ 
+		int num = 666;    //666의 최솟값
+		int count = 1;
+        
+		while(count != input) {    // count가 입력값과 같을 경우 탈출
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+}
