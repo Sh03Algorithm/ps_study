ad4e072268a90fcdf5f6dda584f0102522843945
sinsang2752 <sinsang2752@naver.com>
2024-03-30 15:38:40 +0900

    Update Ps_11728_현상.java

--- week_01/04_Two_Pointer/Ps_11728_현상.java
+++ week_01/04_Two_Pointer/Ps_11728_현상.java
@@ -1,1 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n + m];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < m; i++) {
+			arr[n + i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int r: arr) {
+            // System.out.print(r + "");  <- 시간초과...
+			sb.append(r + " ");
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
