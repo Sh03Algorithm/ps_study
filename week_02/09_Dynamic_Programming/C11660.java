8b20e28dbc1f9576d7a6ec1118aeaa49c03f88d7
KELKEK <35225891+KELKEK@users.noreply.github.com>
2024-04-08 17:00:08 +0900

    Add files via upload

---
+++ week_02/09_Dynamic_Programming/C11660.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class C11660 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[N+1][N+1];
+		int[][] arr2 = new int[N+1][N+1];
+		
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for(int j = 1; j <= N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				arr2[i][j] = arr2[i][j-1] + arr2[i-1][j] - arr2[i-1][j-1] + arr[i][j];
+			}	
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int x1, y1, x2, y2; // (x2,y2) - (x1,y1)
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+			
+			int result = arr2[x2][y2] - arr2[x1-1][y2] - arr2[x2][y1-1] + arr2[x1-1][y1-1];
+			System.out.println(result);
+		}
+	}
+}
