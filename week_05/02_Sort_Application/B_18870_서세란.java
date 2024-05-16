ce75b6ec502fa8d7f04e2a31bef520a255f27f5d
imrani-i <77003079+imrani-i@users.noreply.github.com>
2024-05-16 18:34:10 +0900

    Update B_18870_서세란.java

--- week_05/02_Sort_Application/B_18870_서세란.java
+++ week_05/02_Sort_Application/B_18870_서세란.java
@@ -10,12 +10,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
+    
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
+        // 배열 정렬 
         int[] sortedArr = arr.clone(); 
         Arrays.sort(sortedArr);
 
+        // map에 넣으면서 중복값 제거 + 순서대로 넣기
         Map<Integer, Integer> indexMap = new HashMap<>();
         int index = 0;
         for (int i = 0; i < N; i++) {
@@ -23,7 +24,7 @@ public class Main {
                 indexMap.put(sortedArr[i], index++);
             }
         }
-
+        // 배열을 돌면서 해당 원소를 키로 사용해서 값 불러오기
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             int value = indexMap.get(arr[i]);
