b0c8a6af9ea760eee6a9ac101d4491fa51f46a46
jae <138433808+yjqe@users.noreply.github.com>
2024-05-16 17:02:04 +0900

    Update B_2805_조윤재.java

--- week_05/03_Binary_Search/B_2805_조윤재.java
+++ week_05/03_Binary_Search/B_2805_조윤재.java
@@ -15,6 +15,7 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int left = 0;
 		int right = -1;
+		//나무 n개 중 최대 나무길이
 		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 			right = Math.max(right, arr[i]);
@@ -24,11 +25,12 @@ public class Main {
 			int mid = (left+right)/2;
 			long tree=0;
 			for(int i=0; i<n; i++) {
+				 //나무 자르기
 				if(arr[i]>mid) tree+= arr[i]-mid;
 			}
-			if(tree>=m) {
+			if(tree>=m) {		//너무 많이 자름
 				left = mid+1;
-			}else if(tree<m) { 
+			}else if(tree<m) { 	//나무가 부족함
 				right = mid-1;
 			}
 		}
